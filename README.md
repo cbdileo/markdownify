93c34cd33096fcea4d53cc4bf73a450ef788d0e0
Carleton DiLeo <cbdileo@gmail.com>
2014-11-23 18:10:56 -0800

    Intial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Markdownify
+------
+Markdownify is a jQuery plugin that makes any input/textarea a markdown
+editing field.
+
+## Install
+------
+`$ bower install markdownify`
+
+## [View Demo](http://codeshoppe.io/markdownify/example/portfolio.html)
+![](http://g.recordit.co/8frgy8tW1G.gif)
+- Demo shows markdownify being used as a profile editor
+
+## Requirements
+- [Marked](https://github.com/chjj/marked)
---
+++ src/jquery.markdownify.js
@@ -0,0 +1,40 @@
+(function($){
+  "use strict";
+
+  $.fn.markdownify = function(options) {
+    return this.each(function() {
+      var workspace = $(this),
+      hasRequiredLibraries,
+      settings;
+
+      settings = $.extend({
+        input: workspace.find('textarea'),
+        submit: workspace.find('button'),
+        wrapper: $('<div class="markdown">')
+      }, options);
+
+      hasRequiredLibraries = function () {
+        return typeof marked !== 'undefined' && $.isFunction(marked);
+      };
+
+      settings.submit.click(function() {
+        var newChild,
+        html;
+
+        if (hasRequiredLibraries()) {
+          html = marked(settings.input.val());
+        }
+        else {
+          throw new Error("The JS library 'marked' is required by this plugin");
+        }
+
+        newChild = settings.wrapper.clone().html(html);
+        newChild.hide();
+        workspace.fadeOut('fast', function(){
+          workspace.replaceWith(newChild);
+          newChild.fadeIn();
+        });
+      });
+    });
+  };
+}(jQuery));
